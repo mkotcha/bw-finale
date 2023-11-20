8166c5062d0c144f01e1bc7748c2fefd1a0843f6
Matteo Falasca <matteo.falasca@emmek.org>
2023-11-20 15:53:51 +0100

    fix log

--- src/main/java/org/emmek/bwfinale/payloads/ClientePostDTO.java
+++ src/main/java/org/emmek/bwfinale/payloads/ClientePostDTO.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import org.emmek.bwfinale.entities.Indirizzo;
 
 public record ClientePostDTO(
 
