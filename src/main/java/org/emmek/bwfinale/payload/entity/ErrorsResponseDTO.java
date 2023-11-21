d00a9fbe7e00b25d0f622870e956bce774c1f07e
Matteo Falasca <matteo.falasca@emmek.org>
2023-11-21 18:31:10 +0100

    fix after merge cliente04

--- src/main/java/org/emmek/bwfinale/payload/entity/ErrorsResponseDTO.java
+++ src/main/java/org/emmek/bwfinale/payload/entity/ErrorsResponseDTO.java
@@ -3,6 +3,5 @@ package org.emmek.bwfinale.payload.entity;
 import java.util.Date;
 
 public record ErrorsResponseDTO(String message, Date timestamp) {
-    public static record ErrorsResponseDTO(String message, Date timestamp) {
-    }
+   
 }
--- src/main/java/org/emmek/bwfinale/repositories/ComuneRepository_LOCAL_62198.java
+++
@@ -1,9 +0,0 @@
-package org.emmek.bwfinale.repositories;
-
-import org.emmek.bwfinale.entities.Comune;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ComuneRepository extends JpaRepository<Comune,Long> {
-}
