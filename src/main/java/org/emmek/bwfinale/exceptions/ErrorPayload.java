f6ff1cf7bc08a035ddd723e2d9b12d6edce0d6e1
Matteo Falasca <matteo.falasca@emmek.org>
2023-11-23 12:00:19 +0100

    disastro

---
+++ src/main/java/org/emmek/bwfinale/exceptions/ErrorPayload.java
@@ -0,0 +1,18 @@
+package org.emmek.bwfinale.exceptions;
+
+
+import io.swagger.v3.oas.annotations.Hidden;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@Setter
+@Getter
+@Hidden
+public class ErrorPayload {
+    private String message;
+    private Date timestamp;
+}
---
+++ src/main/java/org/emmek/bwfinale/exceptions/FileUploadExceptionAdvice.java
@@ -0,0 +1,18 @@
+package org.emmek.bwfinale.exceptions;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class FileUploadExceptionAdvice extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity handleMaxSizeException(MaxUploadSizeExceededException exception) {
+        return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage("File Too Large!"));
+    }
+}
